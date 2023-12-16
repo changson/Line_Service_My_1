707312ac427c73ea84844185af226dc073fb115f
changson <changson0823@gmail.com>
2023-12-16 11:21:43 +0800

    Update Line_Service_My_1.py

--- Line_Service_My_1.py
+++ Line_Service_My_1.py
@@ -15,20 +15,28 @@ import openai
 import time
 #======python的函數庫==========
 
-Access_Token = os.environ.get(My_Channel_Access_Token)
-Secret = os.environ.get(My_Channel_Secret)
-Openaiapi_key = os.environ.get(My_Openai_Api_Key)
-
-
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(Access_Token)
-# Channel Secret
-handler = WebhookHandler(Secret)
-# OPENAI API Key初始化設定
-openai.api_key = Openaiapi_key
 
+Access_Token = os.environ.get(My_Channel_Access_Token)
+Secret = os.environ.get(My_Channel_Secret)
+Openaiapi_Key = os.environ.get(My_Openai_Api_Key)
+
+# Channel Access Token
+if Access_Token is not None:
+    line_bot_api = LineBotApi(Access_Token)
+else:
+    app.logger.info("Channel_Access_Token missed"  
+# Channel Secret
+if Secret is not None:
+    handler = WebhookHandler(Secret)
+else:
+    app.logger.info("Channel_Secret missed"
+# OPENAI API Key初始化設定
+if Openaiapi_Key is not None:
+    openai.api_key = Openaiapi_Key
+else:
+    app.logger.info("Openai_Api_Key missed"
 
 # 建立關鍵字和回覆內容的對照表，用來儲存不同的回覆內容
 keyword_reply = {

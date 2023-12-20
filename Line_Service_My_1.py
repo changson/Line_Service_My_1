d81f130a5f56949fed1fa1d8ea8e0fbb15f6bfb7
changson <changson0823@gmail.com>
2023-12-20 15:02:12 +0800

    Update Line_Service_My_1.py

--- Line_Service_My_1.py
+++ Line_Service_My_1.py
@@ -15,18 +15,11 @@ import openai
 import time
 #======python的函數庫==========
 
-#======這裡是呼叫的檔案內容=====
-from message import *
-from new import *
-from Function import *
-from mongodb_function import *
-#======這裡是呼叫的檔案內容=====
-
 #======讓render不會睡著======
 import threading 
 import requests
 
-Render_Url = os.environ.get("My_Channel_Render_Url")
+Render_Url = os.environ.get("My_Render_Url")
 def wake_up_heroku():
     while 1==1:
         url = Render_Url + '/heroku_wake_up'
